6b08b0ccf05f59c48071a5ec884e3f672330cf1d
shiv anand <shivakp2111@gmail.com>
2018-05-10 01:47:16 +0530

    added CategoryViewController

--- ListUp/Controller/CategoryViewController.swift
+++ ListUp/Controller/CategoryViewController.swift
@@ -15,7 +15,6 @@ class CategoryViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
