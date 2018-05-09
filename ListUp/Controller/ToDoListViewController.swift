6316ca6b6c4bb5003ac9af5d831d9c03d25e3d31
shiv anand <shivakp2111@gmail.com>
2018-05-09 16:41:03 +0530

    added NSPredicate

--- ListUp/Controller/ToDoListViewController.swift
+++ ListUp/Controller/ToDoListViewController.swift
@@ -179,24 +179,24 @@ extension ToDoListViewController : UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print("search Bar clicked")
+        
+        // print("search Bar clicked")
         
         let request : NSFetchRequest<Item> = Item.fetchRequest()
-        let predicate = NSPredicate(format: "title CONTAINS %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
-        //print(searchBar.text!)
         request.predicate = predicate
         
         let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
         request.sortDescriptors = [sortDescriptor]
         
         do {
-            
             itemArray = try context.fetch(request)
         } catch {
-            
             print("Error fetching data from the context \(error)")
         }
+        
+        tableView.reloadData()
     }
     
 }
