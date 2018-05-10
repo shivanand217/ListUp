8fc1eb21fbfb866e0f558a2dca66cce243c2364e
shiv anand <shivakp2111@gmail.com>
2018-05-10 11:31:49 +0530

    dataSource in CategoryViewController

--- ListUp/Controller/CategoryViewController.swift
+++ ListUp/Controller/CategoryViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categoryArray = [Category]()
+    
+    // For Encoding and Decoding our data to this pre-specified FilePath
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory,
+                                                in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    // shared singleton object of our Coredata context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //print(dataFilePath!)
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        //loadItems(with: request)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
